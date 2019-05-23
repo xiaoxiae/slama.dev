c7bcbe4665d346a4fc9bd7b0da654b9e3368fa33
xiaoxiae <tomas.slama.131@gmail.com>
2019-05-23 21:36:08 +0200

    improve wiki_to_html script

--- scripts/wiki_to_html/__main__.py
+++ scripts/wiki_to_html/__main__.py
@@ -5,9 +5,9 @@ import pypandoc
 from regex import compile, sub, MULTILINE
 
 
-def get_files_with_extension(folder, extension):
-    """Returns a list of the names of files from a folder with the said extension."""
-    return [file for file in listdir(path.join(folder, '.')) if file.endswith(extension)]
+def get_files_with_extension(source, extension):
+    """Returns a list of the names of files from the source folder with the said extension."""
+    return [file for file in listdir(path.join(source, '.')) if file.endswith(extension)]
 
 
 def read_utf8_file(source):
@@ -20,18 +20,20 @@ def write_utf8_file(destination_path, destination_file_name, contents):
     # ensure that the path exists
     makedirs(destination_path, exist_ok=True)
 
+    # write the file
     with codecs.open(path.join(destination_path, destination_file_name), 'w', encoding='utf8') as file:
         file.write(contents)
 
 
-def copy_utf8_file(source_path, destination_path, destionation_file_name):
+def copy_utf8_file(source_path, destination_path, destination_file_name):
     """Copies the specified utf-8 encoded file to the specified destination."""
-    write_utf8_file(destination_path, destionation_file_name, read_utf8_file(source_path))
+    write_utf8_file(destination_path, destination_file_name, read_utf8_file(source_path))
 
 
-def substitute_str(string, substitutions):
-    """Returns a string that has been substituted using all of the specified substitutions.
-    :param substitutions: A list of tuples, where t[0] and t[1] are the 1st and 2nd regex.sub parameters."""
+def perform_regex_substitutions(string, substitutions):
+    """Returns a string that has been substituted using a list of regex substitutions.
+    :param string: The string to be substituted.
+    :param substitutions: A list of tuples, where t[0] and t[1] are the 1st and 2nd regex.sub() parameters."""
 
     for substitution in substitutions:
         string = sub(*substitution, string, flags=MULTILINE)
@@ -39,62 +41,77 @@ def substitute_str(string, substitutions):
     return string
 
 
-def get_config_from_jekyll(source):
-    """Gets the site title and URL from the specified .yml file."""
+def get_config_from_jekyll(source, variable):
+    """Gets a Jekyll variable from an .yml file."""
     contents = read_utf8_file(source)
 
-    title = compile(r'^title: *?" *(.+?) *"', flags=MULTILINE).search(contents).group(1)
-    url = compile(r'^url: *?" *(.+?) *"', flags=MULTILINE).search(contents).group(1)
+    return compile(r'^' + variable + r': *?" *(.*?) *"', flags=MULTILINE).search(contents).group(1)
 
-    return [title, url]
+
+def substitute_liquid_variables(content, file_name, variables):
+    """Substitute the liquid-like variables."""
+    # add the special section title variable to the mix
+    all_variables = {**variables, **{"section_title": file_name}}
+    
+    for variable, substitution in all_variables.items():
+        content = sub(r"\{\{ *" + variable + r" *\}\}", substitution, content, flags=MULTILINE)
+
+    return content
 
 
 # SCRIPT VARIABLES
+# path to this script
 script_location = path.dirname(path.abspath(__file__))
 
-# folders
-input_folder = path.join(path.expanduser('~'), "Documents", "Wiki")
-output_folder = path.join(script_location, "..", "..", "_site", "subdom", "wiki")
+# generation-related folders
 includes_folder = path.join(script_location, "includes")
+site_folder = path.join(script_location, "..", "..")
 
-# page settings
-site_title, site_url = get_config_from_jekyll(path.join(script_location, '..', '..', '_config.yml'))
-page_title = ""
+wiki_folder = path.join(path.expanduser('~'), "Documents", "Wiki")
+output_folder = path.join(site_folder, "_site", "subdom", "wiki")
+
+# files used in the conversion
+site_config_file = path.join(site_folder, '_config.yml')
+
+# liquid-like variables used in the conversion
+site_variables = {
+    "description": "Tom's personal wiki.",
+    "website_title": get_config_from_jekyll(site_config_file, "title"),
+    "url": get_config_from_jekyll(site_config_file, "wiki-url")
+}
+
 page_structure = "{head}\n<body>\n{content}\n</body>"
 
 # substitutions for Markdown
 md_substitutions = [
-    (r'(^[^-#\s].*?\n)-', '\g<1>\n-')
+    (r'(^[^-#\s].*?\n)-', r'\g<1>\n-')  # prevent incorrect Pandoc formatting of lists after text
 ]
 
 # substitutions for HTML
 html_substitutions = [
-    (r'<li><p>(.+)<\/p><\/li>', '<li>\g<1></li>'),  # fix for incorrectly formatted list items
-    (r'\[\[(.+?)\]\]', '<a href="\g<1>.html">\g<1></a>'),  # wiki links to relative links
-    (r'{{ *(.+?) *?}}', lambda x: site_title if x.group(1) == "site_title" \
-        else page_title if x.group(1) == "page_title" \
-        else site_url if x.group(1) == "page_url" \
-        else "")  # liquid-like tags
+    (r'<li><p>(.+)<\/p><\/li>', r'<li>\g<1></li>'),  # fix for incorrectly formatted list items
+    (r'\[\[(.+?)\]\]', r'<a href="\g<1>.html">\g<1></a>'),  # wiki links to relative links
 ]
 
-for md_file_name in get_files_with_extension(input_folder, ".md"):
-    # read the markdown file
-    md_content = read_utf8_file(path.join(input_folder, md_file_name))
-    md_content = substitute_str(md_content, md_substitutions)
+# CONVERSION
+for md_file_name in get_files_with_extension(wiki_folder, ".md"):
+    # read the markdown file and perform regex substitutions
+    md_content = read_utf8_file(path.join(wiki_folder, md_file_name))
+    md_content = perform_regex_substitutions(md_content, md_substitutions)
 
-    # convert the markdown file to pandoc
-    head = read_utf8_file(path.join(includes_folder, "head.html"))
+    # convert the file to HTML using Pandoc
     html_content = pypandoc.convert_text(md_content, "html", format="md")
 
-    # file + the contents
+    # add additional content (head,...) and perform regex substitutions
+    head = read_utf8_file(path.join(includes_folder, "head.html"))
     html_file_content = page_structure.format(head=head, content=html_content)
-    html_file_content = substitute_str(html_file_content, html_substitutions)
+    html_file_content = perform_regex_substitutions(html_file_content, html_substitutions)
 
-    # the name of the article
-    page_title = md_file_name[:-3]
+    #  swap for liquid-like variables
+    html_file_content = substitute_liquid_variables(html_file_content, md_file_name[:-3].lower(), site_variables)
 
     # write the html file
-    write_utf8_file(output_folder, page_title + ".html", html_file_content)
+    write_utf8_file(output_folder, md_file_name[:-3] + ".html", html_file_content)
 
 # copy the CSS style sheet to the output folder
 copy_utf8_file(path.join(includes_folder, "main.css"), path.join(output_folder, "assets"), "main.css")
