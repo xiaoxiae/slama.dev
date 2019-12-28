79328e9e6eec8b35cb4441fb91888f4bbcaa8b9e
xiaoxiae <tomas.slama.131@gmail.com>
2019-12-28 15:46:28 +0100

    update math support
    Everything is local now. Also, has_math is no longer used, since it's quite annoying and doesn't save all that much data.

--- _plugins/katex.rb
+++ _plugins/katex.rb
@@ -3,28 +3,19 @@ require 'execjs'
 module Jekyll
 	module Tags
 		class KatexBlock < Liquid::Block
-		
+
+			PATH_TO_JS = "./assets/js/katex.min.js"
+
 			def initialize(tag, markup, tokens)
 				super
-				@tag = tag
-				@tokens = tokens
-				@markup = markup
-				
+
 				@display = markup.include? 'display'
 			end
-			
-			PATH_TO_JS = "./assets/js/katex.min.js"
-			
+
 			def render(context)
 				@katex = ExecJS.compile(open(PATH_TO_JS).read)
-				
-				latex_source = super
-				
-				if @display
-						return @katex.call("katex.renderToString", latex_source, {displayMode: true})
-				else
-						return @katex.call("katex.renderToString", latex_source)
-				end
+
+				return @katex.call("katex.renderToString", super, {displayMode: @display})
 			end
 		end
 	end
