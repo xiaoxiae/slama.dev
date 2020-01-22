a4ab7effae8bd62396e4d0794b4c9314aaf59670
xiaoxiae <tomas.slama.131@gmail.com>
2020-01-22 20:32:45 +0100

    minor _sass and config updates

--- _plugins/katex.rb
+++ _plugins/katex.rb
@@ -1,24 +1,24 @@
 require 'execjs'
 
 module Jekyll
-	module Tags
-		class KatexBlock < Liquid::Block
+  module Tags
+    class KatexBlock < Liquid::Block
 
-			PATH_TO_JS = "./assets/js/katex.min.js"
+      PATH_TO_JS = "./assets/js/katex.min.js"
 
-			def initialize(tag, markup, tokens)
-				super
+      def initialize(tag, markup, tokens)
+        super
 
-				@display = markup.include? 'display'
-			end
+        @display = markup.include? 'display'
+      end
 
-			def render(context)
-				@katex = ExecJS.compile(open(PATH_TO_JS).read)
+      def render(context)
+        @katex = ExecJS.compile(open(PATH_TO_JS).read)
 
-				return @katex.call("katex.renderToString", super, {displayMode: @display})
-			end
-		end
-	end
+        return @katex.call("katex.renderToString", super, {displayMode: @display})
+      end
+    end
+  end
 end
 
 Liquid::Template.register_tag('latex', Jekyll::Tags::KatexBlock)
