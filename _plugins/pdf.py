fb6444413ef377a92622002c4c74db75b9c573a9
xiaoxiae <tomas@slama.dev>
2021-07-11 12:50:17 +0200

    PDF: basic kód pdf.py skriptu

--- _plugins/pdf.py
+++ _plugins/pdf.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
+# TODO:
+# - proper margins
+# - TOC
+# - title page
+
 import sys
-from liquid import *
+import tempfile
+import os
+from subprocess import Popen, PIPE
 
-path = sys.argv[1]
-name = sys.argv[2]
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-print(path, name)
+path = sys.argv[1]  # path to the file
+name = sys.argv[2]  # name of the post
+pdf_path = os.path.join("..", "assets", name.strip("/").split("/")[-1] + ".pdf")  # resulting pdf path
 
 with open(path) as f:
     contents = f.read()
 
-env = Environment(
-    undefined=StrictUndefined,
-)
+tmp = tempfile.NamedTemporaryFile(mode="w")
+tmp.write(contents)
 
-template = env.from_string(contents)
-
-print(template.render())
+print(Popen(["pandoc", "-f", "markdown+tex_math_single_backslash", "-i", path, "-o", pdf_path, "--pdf-engine=lualatex"], stdin=PIPE, stdout=PIPE))
