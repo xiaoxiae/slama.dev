8b4e77ffd080dfee049696ae961c92c5ca245811
xiaoxiae <tomas@slama.dev>
2022-06-04 10:36:16 +0200

    CV: automatické 'git submodule foreach git pull' při make

--- _plugins/hooks.rb
+++ _plugins/hooks.rb
@@ -22,6 +22,8 @@ Jekyll::Hooks.register :site, :after_init do |site|
   }
 
   print "          Projects: " + `_plugins/projects.py`
+
+  print "                CV: " + `git submodule foreach git pull`
   print "                CV: " + `_plugins/cv/cv.py --pdf -c`
   print "                CV: " + `_plugins/cv/cv.py --html -o _includes/cv`
 end
