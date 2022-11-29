6036a9f20b385a35359f93f62c95fae7ff497d12
xiaoxiae <tomas@slama.dev>
2022-11-29 21:12:16 +0100

    Lezení: nějaký další drobnosti

--- _plugins/climbing_journal.py
+++ _plugins/climbing_journal.py
@@ -128,6 +128,9 @@ for entry in reversed(sorted(list(journal))):
 
         entry_videos = []
         for video in videos:
+            if 'ignored' in videos[video] and videos[video]['ignored']:
+                continue
+
             if videos[video]["date"] == entry and videos[video]["color"] == color:
                 entry_videos.append(video)
 
@@ -168,9 +171,9 @@ for entry in reversed(sorted(list(journal))):
         if len(entry_videos) != 0:
             line += (
                 " ["
-                + ", ".join(
+                + "".join(
                     [
-                        f"<a class='climbing-link' href='/climbing/videos/{name}'>{videos[name]['label'] if 'label' in videos[name] else 'F' if 'attempts' in videos[name] and videos[name]['attempts'] == 1 else 'A'}</a>"
+                        f"<a class='climbing-link' href='/climbing/videos/{name}'>{'F' if 'attempts' in videos[name] and videos[name]['attempts'] == 1 else 'A'}</a>"
                         for i, name in enumerate(entry_videos)
                     ]
                 )
