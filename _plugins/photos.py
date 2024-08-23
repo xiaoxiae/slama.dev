931abd8a9b3916ba2ee40c644c49aa54483698db
xiaoxiae <tomas@slama.dev>
2024-08-23 10:44:08 +0200

    Photos!

--- _plugins/photos.py
+++ _plugins/photos.py
@@ -11,125 +11,160 @@ OUTPUT = "../_includes/photos.md"
 # Define the list of image information
 images = [
     {
+        "date": datetime(2024, 8, 22),
+        "path": Path('2024-08-22-duck.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C--dPuqtePV/"
+    },
+    {
+        "date": datetime(2024, 8, 19),
+        "path": Path('2024-08-19-frog.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C-2xNP6tQNo/"
+    },
+    {
+        "date": datetime(2024, 8, 18),
+        "path": Path('2024-08-18-lizard.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C-0I-8KNd2f/"
+    },
+    {
+        "date": datetime(2024, 8, 16),
+        "path": Path('2024-08-16-birdhouse.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C-5Xo_fNDkg/"
+    },
+    {
+        "date": datetime(2024, 8, 12),
+        "path": Path('2024-08-12-moon.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C-k_hBBtLsK/"
+    },
+    {
+        "date": datetime(2024, 8, 11),
+        "path": Path('2024-08-11-protab.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C-iEwWLt4nI/"
+    },
+    {
+        "date": datetime(2024, 8, 7),
+        "path": Path('2024-08-07-cat.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C-X7nbfiEKq/"
+    },
+    {
         "date": datetime(2024, 7, 29),
-        "path": Path('DSC6709.jpg'),
+        "path": Path('2024-07-29-berd.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-DNxsUN-di/"
     },
     {
         "date": datetime(2024, 7, 26),
-        "path": Path('tatry-1.jpg'),
+        "path": Path('2024-07-26-tatry-1.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9m__DOtQt-/?img_index=3"
     },
     {
         "date": datetime(2024, 7, 26),
-        "path": Path('tatry-3.jpg'),
+        "path": Path('2024-07-26-tatry-3.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9m__DOtQt-/?img_index=2"
     },
     {
         "date": datetime(2024, 7, 26),
-        "path": Path('tatry-4.jpg'),
+        "path": Path('2024-07-26-tatry-4.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9m__DOtQt-/?img_index=1"
     },
     {
         "date": datetime(2024, 7, 22),
-        "path": Path('DSC9529.jpg'),
+        "path": Path('2024-07-22-vrat.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9kXxFKN3-s/"
     },
     {
         "date": datetime(2024, 7, 18),
-        "path": Path('DSC7572.jpg'),
+        "path": Path('2024-07-18-squirrel.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9uq4K5tiBR/?img_index=2"
     },
     {
         "date": datetime(2024, 7, 14),
-        "path": Path('DSC8604.jpg'),
+        "path": Path('2024-07-14-moon.jpg'),
         "instagram_url": "https://www.instagram.com/p/C97fajytWD1/"
     },
     {
         "date": datetime(2024, 7, 9),
-        "path": Path('austria-1.jpg'),
+        "path": Path('2024-07-09-austria.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9NV0xKNI1s/?img_index=1"
     },
     {
         "date": datetime(2021, 10, 31),
-        "path": Path('prague-4.jpg'),
+        "path": Path('2021-10-31-prague-4.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=4"
     },
     {
         "date": datetime(2021, 10, 31),
-        "path": Path('prague-3.jpg'),
+        "path": Path('2021-10-31-prague-3.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=3"
     },
     {
         "date": datetime(2021, 10, 31),
-        "path": Path('prague-2.jpg'),
+        "path": Path('2021-10-31-prague-2.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=2"
     },
     {
         "date": datetime(2021, 10, 31),
-        "path": Path('prague-1.jpg'),
+        "path": Path('2021-10-31-prague-1.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=1"
     },
     {
         "date": datetime(2020, 10, 2),
-        "path": Path('p2.jpeg'),
+        "path": Path('2020-10-02-panorama-2.jpeg'),
     },
     {
         "date": datetime(2020, 10, 2),
-        "path": Path('p1.jpeg'),
+        "path": Path('2020-10-02-panorama-1.jpeg'),
     },
     {
         "date": datetime(2020, 10, 2),
-        "path": Path('winter-5.jpg'),
+        "path": Path('2020-10-02-winter.jpg'),
     },
     {
         "date": datetime(2018, 6, 27),
-        "path": Path('fire-1.jpg'),
+        "path": Path('2018-06-27-fire.jpg'),
         "instagram_url": "https://www.instagram.com/p/Bkg1_sKFDB4/"
     },
     {
         "date": datetime(2017, 12, 25),
-        "path": Path('christmas-2018-1.jpg'),
+        "path": Path('2017-12-25-christmas.jpg'),
         "instagram_url": "https://www.instagram.com/p/BdI2KnOH_If/?img_index=4"
     },
     {
         "date": datetime(2017, 12, 25),
-        "path": Path('winter-4.jpg'),
+        "path": Path('2017-12-25-winter.jpg'),
         "instagram_url": "https://www.instagram.com/p/BcgD6DYlo57/"
     },
     {
         "date": datetime(2017, 7, 3),
-        "path": Path('other-1.jpg'),
+        "path": Path('2017-07-03-other.jpg'),
         "instagram_url": "https://www.instagram.com/p/BWFzjsglya6/"
     },
     {
         "date": datetime(2017, 2, 26),
-        "path": Path('winter-1.jpg'),
+        "path": Path('2017-02-26-winter.jpg'),
         "instagram_url": "https://www.instagram.com/p/BH7PSl9AS5a/"
     },
     {
         "date": datetime(2016, 7, 16),
-        "path": Path('sunset-2.jpg'),
+        "path": Path('2016-07-16-sunset.jpg'),
         "instagram_url": "https://www.instagram.com/p/BH7PSl9AS5a/"
     },
     {
         "date": datetime(2016, 6, 30),
-        "path": Path('sunset-1.jpg'),
+        "path": Path('2016-06-30-sunset.jpg'),
         "instagram_url": "https://www.instagram.com/p/BHRgCv1AAVY/"
     },
     {
         "date": datetime(2015, 6, 8),
-        "path": Path('flower-1.jpg'),
+        "path": Path('2015-06-08-flower-2.jpg'),
         "instagram_url": "https://www.instagram.com/p/3rpA4whHD7/"
     },
     {
         "date": datetime(2015, 6, 8),
-        "path": Path('flower-2.jpg'),
+        "path": Path('2015-06-08-flower-1.jpg'),
         "instagram_url": "https://www.instagram.com/p/3rWUaeBHMQ/"
     },
     {
         "date": datetime(2015, 6, 6),
-        "path": Path('bee-2.jpg'),
+        "path": Path('2015-06-06-bee.jpg'),
         "instagram_url": "https://www.instagram.com/p/3k9LqVBHM5/"
     },
 ]
--- photos.md
+++ photos.md
@@ -12,6 +12,8 @@ redirect_from:
 Below are collections of photos that I've taken -- some are meant to be just pretty, others depict places that I've seen and things that I've done.
 All of them had been compressed (quite aggressively) to be web-usable; **if you'd like to see the originals, simply click on the download icon.**
 
+I also try to be active on **[Instagram <i class="fa-brands fa-instagram"></i>](https://www.instagram.com/tomas.slama/)**, so feel free to give me a follow if you like the photos! 🙂
+
 _All of the photos are licensed under **[CC-BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)**, so kindly respect it if you plan on using any of them for uses other than personal._
 
 ### Trips
