e28b2d1a11fe325c156d950449b3c353c1eedc61
xiaoxiae <tomas@slama.dev>
2024-09-15 22:11:37 +0200

    Photo stuff

--- _plugins/photos.py
+++ _plugins/photos.py
@@ -11,207 +11,186 @@ OUTPUT = "../_includes/photos.md"
 # Define the list of image information
 images = [
     {
-        "date": datetime(2024, 9, 12),
+        "path": Path('2024-09-23-austria-5-mountains.jpg'),
+    },
+    {
+        "path": Path('2024-09-21-austria-4-clouds.jpg'),
+    },
+    {
+        "path": Path('2024-09-19-austria-3-water.jpg'),
+    },
+    {
+        "path": Path('2024-09-17-austria-2-stars.jpg'),
+    },
+    {
+        "path": Path('2024-09-15-austria-1-sunset.jpg'),
+    },
+    {
         "path": Path('2024-09-12-romania-10-catto.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C_8VcJhtyq6/"
     },
     {
-        "date": datetime(2024, 9, 11),
         "path": Path('2024-09-11-romania-9-forest.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C_yC3rztXDn/"
     },
     {
-        "date": datetime(2024, 9, 10),
         "path": Path('2024-09-10-romania-8-feather.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C_vdfYGtpzB/"
     },
     {
-        "date": datetime(2024, 9, 9),
         "path": Path('2024-09-09-romania-7-doggo.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C_sxVMdNrY2/"
     },
     {
-        "date": datetime(2024, 9, 8),
         "path": Path('2024-09-08-romania-6-doggo.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C_qL6T-NekZ/"
     },
     {
-        "date": datetime(2024, 9, 7),
         "path": Path('2024-09-07-romania-5-statue.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C_nnElXtAw8/"
     },
     {
-        "date": datetime(2024, 9, 6),
         "path": Path('2024-09-06-romania-4-doggo.jpg'),
+        "instagram_url": "https://www.instagram.com/p/C_lC4WINB3E/"
     },
     {
-        "date": datetime(2024, 9, 5),
         "path": Path('2024-09-05-romania-3-crows.jpg'),
         "instagram_url": "https://www.instagram.com/p/C_ic_17Nlvb/"
     },
     {
-        "date": datetime(2024, 9, 4),
         "path": Path('2024-09-04-romania-2-dome.jpg'),
         "instagram_url": "https://www.instagram.com/p/C_f3jZltPjF/"
     },
     {
-        "date": datetime(2024, 9, 3),
         "path": Path('2024-09-03-romania-1-summit.jpg'),
         "instagram_url": "https://www.instagram.com/p/C_dSzOitq8U/"
     },
     {
-        "date": datetime(2024, 8, 30),
         "path": Path('2024-08-30-cabin.jpg'),
         "instagram_url": "https://www.instagram.com/p/C_TIOcVNtUo/"
     },
     {
-        "date": datetime(2024, 8, 25),
         "path": Path('2024-08-25-frog.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-2xNP6tQNo/"
     },
     {
-        "date": datetime(2024, 8, 22),
         "path": Path('2024-08-22-duck.jpg'),
         "instagram_url": "https://www.instagram.com/p/C--dPuqtePV/"
     },
     {
-        "date": datetime(2024, 8, 18),
         "path": Path('2024-08-18-lizard.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-0I-8KNd2f/"
     },
     {
-        "date": datetime(2024, 8, 16),
         "path": Path('2024-08-16-birdhouse.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-5Xo_fNDkg/"
     },
     {
-        "date": datetime(2024, 8, 12),
         "path": Path('2024-08-12-moon.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-k_hBBtLsK/"
     },
     {
-        "date": datetime(2024, 8, 11),
         "path": Path('2024-08-11-protab.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-iEwWLt4nI/"
     },
     {
-        "date": datetime(2024, 8, 7),
         "path": Path('2024-08-07-cat.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-X7nbfiEKq/"
     },
     {
-        "date": datetime(2024, 7, 29),
         "path": Path('2024-07-29-berd.jpg'),
         "instagram_url": "https://www.instagram.com/p/C-DNxsUN-di/"
     },
     {
-        "date": datetime(2024, 7, 26),
         "path": Path('2024-07-26-tatry-1.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9m__DOtQt-/?img_index=3"
     },
     {
-        "date": datetime(2024, 7, 26),
         "path": Path('2024-07-26-tatry-3.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9m__DOtQt-/?img_index=2"
     },
     {
-        "date": datetime(2024, 7, 26),
         "path": Path('2024-07-26-tatry-4.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9m__DOtQt-/?img_index=1"
     },
     {
-        "date": datetime(2024, 7, 22),
         "path": Path('2024-07-22-vrat.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9kXxFKN3-s/"
     },
     {
-        "date": datetime(2024, 7, 18),
         "path": Path('2024-07-18-squirrel.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9uq4K5tiBR/?img_index=2"
     },
     {
-        "date": datetime(2024, 7, 14),
         "path": Path('2024-07-14-moon.jpg'),
         "instagram_url": "https://www.instagram.com/p/C97fajytWD1/"
     },
     {
-        "date": datetime(2024, 7, 9),
         "path": Path('2024-07-09-austria.jpg'),
         "instagram_url": "https://www.instagram.com/p/C9NV0xKNI1s/?img_index=1"
     },
     {
-        "date": datetime(2021, 10, 31),
         "path": Path('2021-10-31-prague-4.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=4"
     },
     {
-        "date": datetime(2021, 10, 31),
         "path": Path('2021-10-31-prague-3.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=3"
     },
     {
-        "date": datetime(2021, 10, 31),
         "path": Path('2021-10-31-prague-2.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=2"
     },
     {
-        "date": datetime(2021, 10, 31),
         "path": Path('2021-10-31-prague-1.jpg'),
         "instagram_url": "https://www.instagram.com/p/CVq_Gw3N-Y_/?img_index=1"
     },
     {
-        "date": datetime(2020, 10, 2),
         "path": Path('2020-10-02-panorama-2.jpeg'),
     },
     {
-        "date": datetime(2020, 10, 2),
         "path": Path('2020-10-02-panorama-1.jpeg'),
     },
     {
-        "date": datetime(2020, 10, 2),
         "path": Path('2020-10-02-winter.jpg'),
     },
     {
-        "date": datetime(2018, 6, 27),
         "path": Path('2018-06-27-fire.jpg'),
         "instagram_url": "https://www.instagram.com/p/Bkg1_sKFDB4/"
     },
     {
-        "date": datetime(2017, 12, 25),
         "path": Path('2017-12-25-christmas.jpg'),
         "instagram_url": "https://www.instagram.com/p/BdI2KnOH_If/?img_index=4"
     },
     {
-        "date": datetime(2017, 12, 25),
         "path": Path('2017-12-25-winter.jpg'),
         "instagram_url": "https://www.instagram.com/p/BcgD6DYlo57/"
     },
     {
-        "date": datetime(2017, 7, 3),
         "path": Path('2017-07-03-other.jpg'),
         "instagram_url": "https://www.instagram.com/p/BWFzjsglya6/"
     },
     {
-        "date": datetime(2017, 2, 26),
         "path": Path('2017-02-26-winter.jpg'),
         "instagram_url": "https://www.instagram.com/p/BH7PSl9AS5a/"
     },
     {
-        "date": datetime(2016, 7, 16),
         "path": Path('2016-07-16-sunset.jpg'),
         "instagram_url": "https://www.instagram.com/p/BH7PSl9AS5a/"
     },
     {
-        "date": datetime(2016, 6, 30),
         "path": Path('2016-06-30-sunset.jpg'),
         "instagram_url": "https://www.instagram.com/p/BHRgCv1AAVY/"
     },
     {
-        "date": datetime(2015, 6, 8),
         "path": Path('2015-06-08-flower-2.jpg'),
         "instagram_url": "https://www.instagram.com/p/3rpA4whHD7/"
     },
     {
-        "date": datetime(2015, 6, 8),
         "path": Path('2015-06-08-flower-1.jpg'),
         "instagram_url": "https://www.instagram.com/p/3rWUaeBHMQ/"
     },
     {
-        "date": datetime(2015, 6, 6),
         "path": Path('2015-06-06-bee.jpg'),
         "instagram_url": "https://www.instagram.com/p/3k9LqVBHM5/"
     },
@@ -222,9 +201,10 @@ results = "<div class='grid'>"
 for image in images:
     icons = ""
 
-    date = image['date']
-    date_string = date.strftime('%Y/%m/%d')
     path = image['path']
+
+    date = datetime.strptime(path.name[:10], '%Y-%m-%d')
+    date_string = date.strftime('%Y/%m/%d')
 
     if date > datetime.now():
         print(f"got future image '{path}' ({date_string} > today), skipping.")
