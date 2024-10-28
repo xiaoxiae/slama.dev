2ad119a83395364742dc8962c8a7721c72156483
xiaoxiae <tomas@slama.dev>
2024-10-28 09:14:27 +0100

    MA 2 notes as a dependency

--- _plugins/hooks.rb
+++ _plugins/hooks.rb
@@ -30,8 +30,13 @@ Jekyll::Hooks.register :site, :after_init do |site|
   }
 
 
-  print "                CV: " + `git submodule foreach git fetch --all`
-  print "                CV: " + `git submodule foreach git reset --hard origin/master`
+  print "             Repos: " + `git submodule foreach git fetch --all`
+
+  print "                MA: " + `git -C _plugins/ma2 reset --hard origin/edit`
+  print "                MA: " + `latexmk -cd -pdf -outdir=Build/ _plugins/ma2/main.tex`
+  print "                MA: " + `cp _plugins/ma2/Build/main.pdf assets/matematicka-analyza-ii.pdf`
+
+  print "                CV: " + `git -C _plugins/cv reset --hard origin/master`
   print "                CV: " + `_plugins/cv/cv.py --pdf -c`
   print "                CV: " + `_plugins/cv/cv.py --html -o _includes/cv`
 end
